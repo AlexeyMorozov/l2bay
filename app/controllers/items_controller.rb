d4e84efbcb849248700140967a9072369c82d813
Rekky <lightrekky@gmail.com>
2012-04-07 01:24:20 +0400

    add new products block

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.paginated_search(params[:query], params) if params[:query]
+    if params[:query]
+      @items = Item.paginated_search(params[:query], params)
+    else
+      @products = Product.includes(:shop).order('shops.seen_time DESC').limit(10).all
+    end
   end
 
   def show
