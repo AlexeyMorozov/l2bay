96c60864198ce6f0aa0038098e1640bef8757aac
Rekky <lightrekky@gmail.com>
2012-04-29 16:59:42 +0400

    devise mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ L2bay::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+
   config.action_mailer.default_url_options = { :host => 'l2bay.com' }
 
   # Disable delivery errors, bad email addresses will be ignored
