9816d90cfe318120a1537e2590a4734d5d4cf546
Rekky <lightrekky@gmail.com>
2012-04-29 17:15:16 +0400

    add mail sender name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "no-reply@l2bay.com"
+  config.mailer_sender = "L2bay <no-reply@l2bay.com>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
