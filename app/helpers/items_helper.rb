4cce80d16ee653a1924a9d3edd4bec98d4a42382
Rekky <lightrekky@gmail.com>
2012-04-30 18:08:50 +0400

    refactor server selector

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,6 @@
 module ItemsHelper
+  def server_options
+    server_urls = Server.all.map {|s| [s.name, url_for(server: s)]}
+    options_for_select(server_urls, url_for(server: current_server))
+  end
 end
