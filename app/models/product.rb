32bfc0b8b911e5d54e7fa8d01cce1272a0ded5bf
Rekky <lightrekky@gmail.com>
2012-04-30 21:45:53 +0400

    truncate product name if too long

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,11 @@ class Product < ActiveRecord::Base
   def item
     Item.find(item_id)
   end
+
+  def to_s
+    str = item.name
+    str += " [#{item.sa}]" if item.sa.present?
+    str += " +#{enchant}" if enchant > 0
+    str
+  end
 end
