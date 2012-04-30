a6088f5a1a4e0a014ff6f62fd6c6bc5e127fbcd0
Rekky <lightrekky@gmail.com>
2012-04-30 18:22:22 +0400

    keep all params while changing server

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,6 +1,12 @@
 module ItemsHelper
   def server_options
-    server_urls = Server.all.map {|s| [s.name, url_for(server: s)]}
-    options_for_select(server_urls, url_for(server: current_server))
+    server_urls = Server.all.map {|s| [s.name, change_server_link(s)]}
+    options_for_select(server_urls, change_server_link)
+  end
+
+  private
+
+  def change_server_link(server = current_server)
+    url_for(params.merge(server: server, page:nil))
   end
 end
