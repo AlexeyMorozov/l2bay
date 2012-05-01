62df7d731c8a32a32ccbfb4e6dca9978dedcf5d9
Rekky <lightrekky@gmail.com>
2012-05-01 14:45:33 +0400

    truncate location

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,7 +6,7 @@ class Shop < ActiveRecord::Base
   attr_accessible :server_id, :player, :pack, :sell, :seen_time, :loc_x, :loc_y, :loc_z
 
   def location_name
-    @location_name ||= Location.find_by_coords(loc_x, loc_y, loc_z).try(:name) || "Look coords"
+    @location_name ||= Location.find_by_coords(loc_x, loc_y, loc_z).try(:name) || "<unnamed>"
   end
 
   def coordinates
