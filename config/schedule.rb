6cc9ffdd2dcd8cec9e28b7d29cd656841698f67e
Rekky <lightrekky@gmail.com>
2012-05-09 01:10:19 +0400

    Run backup with bundler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "#{path}/log/cron.log"
 
 every 6.hours do
-  command "backup perform -t l2bay -c #{path}/Backup/config.rb -l #{path}/log/backup.log"
+  command "cd #{path}; bundle exec backup perform -t l2bay -c #{path}/Backup/config.rb -l #{path}/log/backup.log"
 end
