3bc80454209ddeed7ff1b4b613ba9ca9b3817707
Rekky <lightrekky@gmail.com>
2012-05-09 14:02:30 +0400

    Backup job at one line

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ set :output, "#{path}/log/cron.log"
 
 every 6.hours do
   command <<-CMD
-    cd #{path} &&
+    cd #{path} && \
     bundle exec backup perform -t l2bay -c #{path}/backup/config.rb \
       -l #{path}/log/ --tmp-path #{path}/tmp/backup/ --root-path ../shared/backup
   CMD
