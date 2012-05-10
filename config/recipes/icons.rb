0566d53ad575bc6b9b783a04c4d1ed9857b97628
Rekky <lightrekky@gmail.com>
2012-05-10 21:14:50 +0400

    Prepare icon assets

--- config/recipes/icons.rb
+++ config/recipes/icons.rb
@@ -2,20 +2,24 @@ namespace :icons do
   desc "Upload icons"
   task :upload do
     host = find_servers.first.host
-    run "mkdir -p #{shared_path}/images"
-    run_locally "rsync -rvz app/assets/images/items #{user}@#{host}:#{shared_path}/images/"
+    run "mkdir -p #{shared_path}/icons"
+    run_locally "rsync -rvz app/assets/icons/items #{user}@#{host}:#{shared_path}/icons/"
   end
   after "deploy:setup", "icons:upload"
 
   desc "Symlink icons"
   task :symlink do
-    run "ln -nfs #{shared_path}/images/items #{release_path}/app/assets/images/items"
+    run "ln -nfs #{shared_path}/icons/items #{release_path}/app/assets/icons/items"
   end
   before "deploy:assets:precompile", "icons:symlink" unless precompile_without.include?('icons')
 
-  desc "Append already compiled icon assets"
-  task :append_assets do
-    run "cd #{shared_path}/assets && cat item_icons.yml >> manifest.yml"
+  desc "Prepare icon assets"
+  task :assets do
+    if precompile_without.include?('icons')
+      run "cd #{shared_path}/assets && cat icons.yml >> manifest.yml"
+    else
+      run "cd #{shared_path}/assets && grep '^items\/' manifest.yml > icons.yml"
+    end
   end
-  after "deploy:assets:precompile", "icons:append_assets" if precompile_without.include?('icons')
+  after "deploy:assets:precompile", "icons:assets"
 end
