307302ae903578b2ef8748862c0e859ec8ffb2a0
Rekky <lightrekky@gmail.com>
2012-06-24 23:58:46 +0400

    Keep log logic at Log class

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,19 +3,43 @@ class Log < ActiveRecord::Base
 
   attr_accessible :data, :log_type, :user_id
 
+  Message = Struct.new(:title, :fields)
+  MessageField = Struct.new(:type, :title) do
+    def get_value(value)
+      value
+    end
+  end
+  class ServerField < MessageField
+    def get_value(value)
+      Server.find(value).name
+    end
+  end
+  @@messages = {
+      1 => Message.new('Added points', [
+          ServerField.new('L', 'Server'),
+          MessageField.new('L', 'Total shops'),
+          MessageField.new('L', 'Accepted shops'),
+          MessageField.new('Q', 'Total points'),
+          MessageField.new('Q', 'Earned points'),
+      ]),
+  }
+
   def title
     message.title
   end
 
   def description
-    message.description
+    format = message.fields.map { |f| f.type }.join
+    values = data.unpack(format)
+    messages = message.fields.zip(values).map do |field, value|
+      "#{field.title}: #{field.get_value(value)}."
+    end
+    messages.join(' ')
   end
 
   private
 
   def message
-    m = Message.create(log_type)
-    m.data = data
-    m
+    @@messages[log_type]
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module L2bay
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/lib/messages)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
